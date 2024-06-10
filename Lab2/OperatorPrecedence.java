1058e2057a59925135cf3a356c77c464240ba704
dhvaniii4 <149350047+dhvaniii4@users.noreply.github.com>
2024-06-10 18:36:43 +0530

    Add files via upload
    Lab2

---
+++ Lab2/OperatorPrecedence.java
@@ -0,0 +1,12 @@
+public class OperatorPrecedence{
+    public static void main(String[] args){
+        //int calc1;
+        int calc1=10+20*30;
+        System.out.println("Calculation is "+calc1);
+        int calc2=100/10*100;
+        System.out.println("Calculation is "+calc2);
+        double calc3=5*4/4%3;
+        System.out.println("Calculation is "+calc3);
+        
+    }
+}
